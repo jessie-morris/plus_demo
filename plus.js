ca00f1ba8fefb3bc5f6591159174a3f234aa13f1
Jessie <jmorris2@pillartechnology.com>
2020-01-29 15:52:46 -0500

    changes

--- plus.js
+++ plus.js
@@ -1,15 +1,22 @@
 var canvas = Raphael("container", 750, 500);
 boxWidth = 30
 boxHeight = 50
+largeIndent = 9 * boxWidth
+smallIndent = 8 * boxWidth
+
 counter = 1
 rects = []
 
+const drawBox = (x, y, width, height, color) => {
+   canvas.rect(x, y, width, height).attr({ fill: color }) 
+}
+
 for (var row = 0; row < 4; row++) {
     for (var col = 1; col <= 6; col++) {
-        rects.push(canvas.rect(300 + (boxWidth * col), 0 + (boxHeight * row), boxWidth, boxHeight)
+        rects.push(canvas.rect(largeIndent + (boxWidth * col), 0 + (boxHeight * row), boxWidth, boxHeight)
             .attr({ fill: 'white' }))
         if (col % 3 == 0) {
-            canvas.text(300 + (boxWidth * col) + 25, 0 + (boxHeight * row) + 45, counter)
+            canvas.text(largeIndent + (boxWidth * col) + 25, 0 + (boxHeight * row) + 45, counter)
             counter++;
         }
     }
@@ -17,30 +24,30 @@ for (var row = 0; row < 4; row++) {
 
 for (var row = 0; row < 2; row++) {
     for (var col = 1; col <= 24; col++) {
-        rects.push(canvas.rect(30 + (boxWidth * col), 200 + (row * boxHeight), boxWidth, boxHeight)
+        rects.push(canvas.rect((boxWidth * col), 200 + (row * boxHeight), boxWidth, boxHeight)
             .attr({ fill: 'white' }))
 
         if (col % 3 == 0) {
-            canvas.text(30 + (boxWidth * col) + 25, 200 + (boxHeight * row) + 45, counter)
+            canvas.text((boxWidth * col) + 25, 200 + (boxHeight * row) + 45, counter)
             counter++;
         }
     }
 }
 for (var row = 0; row < 3; row++) {
     for (var col = 1; col <= 6; col++) {
-        rects.push(canvas.rect(300 + (boxWidth * col), 300 + (row * boxHeight), boxWidth, boxHeight)
+        rects.push(canvas.rect(largeIndent + (boxWidth * col), 300 + (row * boxHeight), boxWidth, boxHeight)
             .attr({ fill: 'white' }))
         if (col % 3 == 0) {
-            canvas.text(300 + (boxWidth * col) + 25, 300 + (boxHeight * row) + 45, counter)
+            canvas.text(largeIndent + (boxWidth * col) + 25, 300 + (boxHeight * row) + 45, counter)
             counter++;
         }
     }
 }
 for (var col = 1; col <= 3; col++) {
-    rects.push(canvas.rect(270 + (boxWidth * 2 * col), 450, boxWidth * 2, boxHeight)
+    rects.push(canvas.rect(smallIndent + (boxWidth * 2 * col), 450, boxWidth * 2, boxHeight)
         .attr({ fill: 'white' }))
     if (col % 3 == 0) {
-        canvas.text(300 + (boxWidth * 2 * col) + 25, 450 + 45, counter)
+        canvas.text(smallIndent + (boxWidth * 2 * col) + 25, 450 + 45, counter)
         counter++;
     }
 }
