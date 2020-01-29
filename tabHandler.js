ba2d6c0f63dad004a8988b0403d9f78170d3390d
Cameron Marsh <cmarsh@pillartechnology.com>
2020-01-29 16:01:00 -0500

    Add tabs and styling

---
+++ tabHandler.js
@@ -0,0 +1,19 @@
+function openTab(evnt, tabName) {
+	var tablinks = document.getElementsByClassName("tab-link");
+	const tags = document.getElementsByClassName("active")
+	const contents = document.getElementsByClassName("tab-content")
+	console.log(tags)
+	console.log(tabName)
+	console.log("content values",Object.values(contents))
+
+	for(let i = 0; i < contents.length; i++){
+		contents[i].style = { display: "none" };
+	}
+
+	for(let activeTag of tags) {
+		activeTag.className = activeTag.className.replace(' active', '')
+	}
+
+	document.getElementById(tabName).style.display = 'block';
+	evnt.currentTarget.className += " active";
+}
