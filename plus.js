e1504fdcdc42315953a53ab4a32625889b839406
Jessie <jmorris2@pillartechnology.com>
2020-01-30 13:59:55 -0500

    making boxes smaller so they work better on a horizontal ipad

--- plus.js
+++ plus.js
@@ -1,6 +1,6 @@
-var canvas = Raphael("container", 630, 500);
+var canvas = Raphael("container", 630, 450);
 boxWidth = 25
-boxHeight = 50
+boxHeight = 45
 largeIndent = 9 * boxWidth
 smallIndent = 8 * boxWidth
 
@@ -16,7 +16,7 @@ for (var row = 0; row < 4; row++) {
         rects.push(canvas.rect(largeIndent + (boxWidth * col), 0 + (boxHeight * row), boxWidth, boxHeight)
             .attr({ fill: 'white' }))
         if (col % 3 == 0) {
-            canvas.text(largeIndent + (boxWidth * col) + 20, 0 + (boxHeight * row) + 45, counter)
+            canvas.text(largeIndent + (boxWidth * col) + 20, 0 + (boxHeight * row) + 40, counter)
             counter++;
         }
     }
@@ -24,30 +24,30 @@ for (var row = 0; row < 4; row++) {
 
 for (var row = 0; row < 2; row++) {
     for (var col = 1; col <= 24; col++) {
-        rects.push(canvas.rect((boxWidth * col), 200 + (row * boxHeight), boxWidth, boxHeight)
+        rects.push(canvas.rect((boxWidth * col), (4 * boxHeight) + (row * boxHeight), boxWidth, boxHeight)
             .attr({ fill: 'white' }))
 
         if (col % 3 == 0) {
-            canvas.text((boxWidth * col) + 20, 200 + (boxHeight * row) + 45, counter)
+            canvas.text((boxWidth * col) + 18, (4 * boxHeight) + (boxHeight * row) + 40, counter)
             counter++;
         }
     }
 }
 for (var row = 0; row < 3; row++) {
     for (var col = 1; col <= 6; col++) {
-        rects.push(canvas.rect(largeIndent + (boxWidth * col), 300 + (row * boxHeight), boxWidth, boxHeight)
+        rects.push(canvas.rect(largeIndent + (boxWidth * col), (6 * boxHeight) + (row * boxHeight), boxWidth, boxHeight)
             .attr({ fill: 'white' }))
         if (col % 3 == 0) {
-            canvas.text(largeIndent + (boxWidth * col) + 20, 300 + (boxHeight * row) + 45, counter)
+            canvas.text(largeIndent + (boxWidth * col) + 18, (6 * boxHeight) + (boxHeight * row) + 40, counter)
             counter++;
         }
     }
 }
 for (var col = 1; col <= 3; col++) {
-    rects.push(canvas.rect(smallIndent + (boxWidth * 2 * col), 450, boxWidth * 2, boxHeight)
+    rects.push(canvas.rect(smallIndent + (boxWidth * 2 * col), (9 * boxHeight), boxWidth * 2, boxHeight)
         .attr({ fill: 'white' }))
     if (col % 3 == 0) {
-        canvas.text(smallIndent + (boxWidth * 2 * col) + 20, 450 + 45, counter)
+        canvas.text(smallIndent + (boxWidth * 2 * col) + 43, (9 * boxHeight) + 40, counter)
         counter++;
     }
 }
